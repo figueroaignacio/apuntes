4defa4f4ca61deaefc8c9e01f7087f820a59f29c
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-10 20:15:22 -0300

    feat: add root layout

--- src/components/app-router.tsx
+++ src/components/app-router.tsx
@@ -6,12 +6,17 @@ import { HomePage } from "@/pages/home/home";
 import { NotFoundPage } from "@/pages/not-found/not-found";
 import { NoteDetailsPage } from "@/pages/note-details/note-details";
 
+// Layout
+import { RootLayout } from "@/layout/layout";
+
 export function AppRouter() {
   return (
     <Routes>
-      <Route path="" element={<HomePage />} />
-      <Route path="/note/:slug" element={<NoteDetailsPage />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route element={<RootLayout />}>
+        <Route path="" element={<HomePage />} />
+        <Route path="/note/:slug" element={<NoteDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/layout/layout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+
+export function RootLayout() {
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
