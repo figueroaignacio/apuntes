b77d128928eb15c519df93750caef6250b246d5d
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-12 17:12:08 -0300

    feat: add styles to index

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,34 +3,34 @@ export const indexNotes = [
     cuatrimestre: "Primer Cuatrimestre",
     subjects: [
       {
-        title: "Programación I",
+        title: "Programación I 💻",
         href: "/apuntes/primer-cuatrimestre/programacion-i",
       },
       {
-        title: "Arquitectura y Sistemas Operativos",
+        title: "AySO 🖥️",
         href: "/apuntes/primer-cuatrimestre/ayso",
       },
       {
-        title: "Organización Empresarial",
+        title: "Organización Empresarial 🏢",
         href: "/apuntes/primer-cuatrimestre/organizacion-empresarial",
       },
     ],
   },
-  // {
-  //   cuatrimestre: "Segundo Cuatrimestre",
-  //   subjects: [
-  //     {
-  //       title: "Programación II",
-  //       href: "/apuntes/segundo-cuatrimestre/programacion-II",
-  //     },
-  //     {
-  //       title: "Base de Datos I",
-  //       href: "/apuntes/segundo-cuatrimestre/matematica-II",
-  //     },
-  //     {
-  //       title: "Inglés",
-  //       href: "/apuntes/segundo-cuatrimestre/ingles-tecnico",
-  //     },
-  //   ],
-  // },
+  {
+    cuatrimestre: "Segundo Cuatrimestre (no disponible por el momento)",
+    subjects: [
+      {
+        title: "Programación II 💻",
+        href: "/apuntes/segundo-cuatrimestre/programacion-ii",
+      },
+      {
+        title: "Base de Datos I 🗄️",
+        href: "/apuntes/segundo-cuatrimestre/base-de-datos-i",
+      },
+      {
+        title: "Inglés 🗣️",
+        href: "/apuntes/segundo-cuatrimestre/ingles",
+      },
+    ],
+  },
 ];
