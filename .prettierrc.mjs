52c1662f672e2c2b9c756c47da4d508d568abebb
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-13 23:17:27 -0300

    feat: add better prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,12 +1,31 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ['prettier-plugin-astro'],
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
+        parser: 'astro',
+      },
+    },
+    {
+      files: '*.mdx',
+      options: {
+        parser: 'markdown',
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
       },
     },
   ],
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  arrowParens: 'always',
 };
