2cb7f39b398b5cb1a048ed0848e01873e8893fbf
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-26 18:19:35 -0300

    refactor: add sections folder

--- src/lib/mdx-components.ts
+++ src/lib/mdx-components.ts
@@ -1,4 +1,8 @@
 import AssignatureCard from '@/components/AssignatureCard/AssignatureCard.astro';
 import CardLink from '@/components/CardLink/CardLink.astro';
+import AySO from '@/sections/AySO.astro';
+import Matematica from '@/sections/Matematica.astro';
+import OE from '@/sections/OE.astro';
+import P1 from '@/sections/P1.astro';
 
-export const components = { CardLink, AssignatureCard };
+export const components = { CardLink, AssignatureCard, P1, AySO, OE, Matematica };
---
+++ src/lib/topics.ts
@@ -0,0 +1,100 @@
+export const P1Topics = [
+  {
+    title: 'Estructuras Condicionales',
+    description:
+      'Las estructuras condicionales permiten que nuestros programas tomen decisiones y...',
+    href: '/apuntes/tup/primer-cuatrimestre/programacion-i/temas/estructuras-condicionales',
+  },
+  {
+    title: 'Estructuras Repetitivas',
+    description:
+      'Las estructuras repetitivas, también conocidas como bucles o ciclos, permiten ejecutar un bloque de código múltiples veces. Python ofrece principalmente dos tipos de bucles...',
+    href: '/apuntes/tup/primer-cuatrimestre/programacion-i/temas/estructuras-repetitivas',
+  },
+  {
+    title: 'Estructuras Secuenciales',
+    description:
+      'Las estructuras secuenciales son la forma más básica de programación. En ellas, las instrucciones se ejecutan una tras...',
+    href: '/apuntes/tup/primer-cuatrimestre/programacion-i/temas/estructuras-secuenciales',
+  },
+  {
+    title: 'Trabajo Colaborativo',
+    description: 'El mundo de Git & GitHub',
+    href: '/apuntes/tup/primer-cuatrimestre/programacion-i/temas/trabajo-colaborativo',
+  },
+  {
+    title: 'Listas',
+    description:
+      'Las listas son estructuras de datos fundamentales en Python que permiten almacenar múltiples elementos de forma ordenada. A diferencia de los arrays tradicionales en otros lenguajes, las listas en Python ofrecen mayo...',
+    href: '/apuntes/tup/primer-cuatrimestre/programacion-i/temas/listas',
+  },
+  {
+    title: 'Funciones',
+    description:
+      'Una función es un bloque de código reutilizable diseñado para realizar una tarea específica. Es uno de los conceptos fundamentales en programación y...',
+    href: '/apuntes/tup/primer-cuatrimestre/programacion-i/temas/funciones',
+  },
+];
+
+export const AySOTopics = [
+  {
+    title: 'Introducción a la arquitectura de computadoras',
+    description:
+      'La arquitectura de computadoras estudia la estructura, organización y funcionamiento interno de los sistemas computaci...',
+    href: '/apuntes/tup/primer-cuatrimestre/ayso/temas/introduccion-a-la-arquitectura-de-computadoras',
+  },
+  {
+    title: 'Introducción a los sistemas operativos',
+    description:
+      'Un sistema operativo (SO) es el software fundamental que actúa como intermediario entre el hardware de la computadora y...',
+    href: '/apuntes/tup/primer-cuatrimestre/ayso/temas/introduccion-a-los-sistemas-operativos',
+  },
+  {
+    title: 'Introducción a scripting (Bash)',
+    description:
+      'Bash (Bourne Again SHell) es un intérprete de comandos y lenguaje de programación de scripts que sirve como interfaz entre el usuario y...',
+    href: '/apuntes/tup/primer-cuatrimestre/ayso/temas/introduccion-a-scripting-bash',
+  },
+];
+
+export const OETopics = [
+  {
+    title: 'Organizaciones',
+    description:
+      'Una organización es un sistema social compuesto por individuos o grupos que, teniendo valores compartidos, se interrelacionan y utilizan recursos con los que...',
+    href: '/apuntes/tup/primer-cuatrimestre/organizacion-empresarial/temas/organizaciones',
+  },
+  {
+    title: 'Empresas',
+    description:
+      'Un sistema operativo (SO) es el software fundamental que actúa como intermediario entre el hardware de la computadora y...',
+    href: '/apuntes/tup/primer-cuatrimestre/organizacion-empresarial/temas/empresas',
+  },
+  {
+    title: 'Áreas Típicas',
+    description:
+      'Las empresas son organizaciones dedicadas a los negocios con fines de lucro. Son unidades de producción que coordinan factores productivos para crear...',
+    href: '/apuntes/tup/primer-cuatrimestre/organizacion-empresarial/temas/areas-tipicas',
+  },
+];
+
+export const MateTopics = [
+  {
+    title: 'Álgebra de Boole',
+    description:
+      'El Álgebra de Boole es un sistema matemático que trabaja con variables que pueden tomar únicamente dos valores, 0 (falso) y 1 (verdadero). Esta característica la hace...',
+    href: '/apuntes/tup/primer-cuatrimestre/matematica/temas/algebra-de-boole',
+  },
+  {
+    title: 'Sistema Binario',
+    description:
+      'El sistema binario es un sistema de numeración que utiliza solo dos dígitos, 0 y 1. Es la base del funcionamiento de todos los dispositivos electrónicos y...',
+    href: '/apuntes/tup/primer-cuatrimestre/matematica/temas/sistema-binario',
+  },
+  {
+    title: 'Lógica',
+    description:
+      'La lógica en matemáticas, como materia, trata del estudio de las reglas del razonamiento válido. Es una herramienta fundamental para...',
+    href: '/apuntes/tup/primer-cuatrimestre/matematica/temas/logica',
+  },
+];
